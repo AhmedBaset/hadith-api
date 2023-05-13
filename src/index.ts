959565208e60580f59412a5d72a86e0c2451beba
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-05-13 22:16:53 +0300

    Setup API

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
-import chalk from "chalk";
 
 dotenv.config();
 
 import { client } from "./mongodb.js";
-
 import { errorLogger, logEvents, logger } from "./middlewares/logger.js";
+import { createMongoDbIndexes } from "./helpers/createIndexes.js";
 
 import { notFoundRouter } from "./routes/notFound.js";
 import { bookRouter } from "./routes/book.js";
 import { chapterRouter } from "./routes/chapter.js";
+import { hadithRouter } from "./routes/hadith.js";
+import { docsRoute } from "./routes/docs.js";
 
 const app = express();
 
@@ -25,26 +26,29 @@ app.use(logger);
 //* Routes
 app.use(chapterRouter);
 app.use(bookRouter);
+app.use(hadithRouter);
+app.use("/docs", docsRoute);
 
 app.use(notFoundRouter);
 
 app.use(errorLogger);
 
+// Start the server
 const PORT = process.env.PORT || 3500;
 
 client.connect().then(() => {
 	app.listen(PORT, async () => {
-		console.log(
-			chalk.cyan(`Server is running on port ${chalk.cyanBright(PORT)}.`)
-		);
+		console.log(`Server is running on port ${PORT}.`);
 	});
 });
+
+createMongoDbIndexes();
 
 client.on("error", (err) => {
 	console.error(err);
 	logEvents("MongoDbErrors.log", [
+		new Date().toISOString(),
 		err.name,
 		err.message,
-		new Date().toLocaleString(),
 	]);
 });
